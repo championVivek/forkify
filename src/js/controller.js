2d2952558ac56480c3cd19c5a462b2eebc034efe
championVivek <vivekkumarse@gmail.com>
2022-08-30 23:21:10 +0530

    showrecipe api added

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,3 +1,5 @@
+require('dotenv').config()
+
 const recipeContainer = document.querySelector('.recipe');
 
 const timeout = function (s) {
@@ -8,6 +10,31 @@ const timeout = function (s) {
   });
 };
 
-// https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
+
+const showRecipe = async function () {
+  try {
+    const res = await fetch(`https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886?key=${process.env.API_KEY}`)
+    const data = await res.json();
+
+    if (!res.ok)
+      throw new Error(`${data.message} ${res.status}`)
+    let { recipe } = data.data
+    recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceUrl: recipe.source_url,
+      image: recipe.image_url,
+      servings: recipe.servings,
+      cookingTime: recipe.cooking_time,
+      ingredients: recipe.ingredients
+    }
+    console.log(recipe);
+  } catch (e) {
+    alert(e)
+  }
+}
+
+showRecipe()
